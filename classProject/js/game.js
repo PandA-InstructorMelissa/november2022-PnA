a708d95a4442d21c1bb1ce61f17c2ab1b762e381
Melissa Longenberger <mlongenberger@codingdojo.com>
2022-11-10 20:04:57 -0500

    saving classProject

--- classProject/js/game.js
+++ classProject/js/game.js
@@ -1,21 +1,16 @@
 // const {playerWord, correctGuess, rounds} = require('./logic');
 // const {Player} = require('./class/player')
 
+// Defaults Global
 let letterCount = '3'
 let playOneName = 'Jane'
 let playTwoName = 'Bill'
 
-function setUpGame(e){
-    e.preventDefault()
-    letterCount = document.wordChoice.letterCount.value
-    playOneName = document.wordChoice.playerOne.value
-    playTwoName = document.wordChoice.playerTwo.value
-}
 
-function playGame(){
-    // console.log("You started this game")
+function playGame(e){
+    e.preventDefault()
+    setUpGame()
     playerWord(letterCount)
-    // console.log("testing our return of words:", theWords)
     var playing = true
     while (playing) {
         var playOneRandWord =  theWords.playerOneWord //'end' //theWords.playerOneWord
@@ -24,16 +19,8 @@ function playGame(){
         console.log("Player One instance created", playerOne)
         var playerTwo = new Player(playTwoName, playTwoRandWord)
         console.log("Player Two instance created", playerTwo)
-        // build game board
-        var node = document.createElement('div')
-        for (var i = 1; i <=6; i++) {
-            for (var lc = 1; lc <= letterCount; lc++) {
-                var span = document.createElement('span')
-                span.setAttribute('class', 'letter')
-                node.appendChild(span)
-            }
-        }
-        document.getElementById('board').appendChild(node)
+        drawPlayerBoard(playerOne)
+        drawPlayerBoard(playerTwo)
         // Rounds
         for(r=1; r<=6; r++){
             
--- classProject/js/logic.js
+++ classProject/js/logic.js
@@ -6,6 +6,15 @@ var wordBank = {
 
 var letters = {'a': 'white', 'b': 'white', 'c': 'white', 'd': 'white', 'e': 'white', 'f': 'white', 'g': 'white', 'h': 'white', 'i': 'white', 'j': 'white', 'k': 'white', 'l': 'white', 'm': 'white', 'n': 'white', 'o': 'white', 'p': 'white', 'q': 'white', 'r': 'white', 's': 'white','t': 'white', 'u': 'white', 'v': 'white', 'w': 'white', 'x': 'white', 'y': 'white', 'z': 'white'}
 
+
+// Game set up
+function setUpGame(){
+    letterCount = document.wordChoice.letterCount.value
+    playOneName = document.wordChoice.playerOne.value
+    playTwoName = document.wordChoice.playerTwo.value
+}
+
+// Setting the players words
 function playerWord(a=3){ 
     var playerOneWord = choseWord(a)
     var playerTwoWord = choseWord(a)
@@ -19,12 +28,43 @@ function playerWord(a=3){
     return theWords
 }
 
+// Randomizer
 function choseWord(a) {
     var random = Math.floor(Math.random()* wordBank[a].length)
     var theWord = wordBank[a][random]
     return theWord
 }
-
+// build game board
+function drawPlayerBoard(p) {
+    var node = document.createElement('div')
+    node.setAttribute('class', 'boardGuess')
+    var h2 = document.createElement('h2')
+    var name = document.createTextNode(p.name)
+    h2.appendChild(name)
+    node.appendChild(h2)
+    for (var i = 1; i <=6; i++) {
+        var div = document.createElement('div')
+        div.setAttribute('class', 'row')
+        for (var lc = 1; lc <= letterCount; lc++) {
+            var span = document.createElement('span')
+            span.setAttribute('class', 'letter')
+            div.appendChild(span)
+        }
+        node.appendChild(div)
+    }
+    var keys = document.createElement('div')
+    keys.setAttribute('class', 'keyboard')
+    for (var i = 0; i < 26; i++) {
+        var key = document.createElement('span')
+        var value = document.createTextNode(Object.keys(p.keyboard)[i])
+        key.appendChild(value)
+        key.setAttribute('class', Object.values(p.keyboard)[i])
+        keys.appendChild(key)
+    }
+    node.appendChild(keys)
+    document.getElementById('board').appendChild(node)
+}
+// Checking player guess
 function correctGuess(b, c){
     var result = 'Guess Again'
     if(p.guess == p.word) {
@@ -49,6 +89,7 @@ function correctGuess(b, c){
         return result
     }
 }
+// Running of the rounds
 function rounds(p1, p2){
     var OneResult = correctGuess(p1)
     var TwoResult = correctGuess(p2)
